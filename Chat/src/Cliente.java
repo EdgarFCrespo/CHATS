3f973f5c0725247839d2a58d57308aaa908701c0
RociioH <32103075+RociioH@users.noreply.github.com>
2017-11-21 17:53:09 -0600

    clase EnvioLine y metodo windowOpened

--- Chat/src/Cliente.java
+++ Chat/src/Cliente.java
@@ -6,6 +6,17 @@ public class Cliente {
 
 class MarcoCliente extends JFrame{
     public MarcoCliente(){
+        setBounds(600,300,280,350);
+        LaminaMarcoCliente milamina = new LaminaMarcoCliente();
+        add(milamina);
+        setVisible(true);
+        addWindowListener(new EnvioLine());
+    }
+}
+
+//=============Envia señal=====================
+class EnvioLine extends WindowAdapter{
+    public void windowOpened(WindowEvent e){
         
     }
 }
