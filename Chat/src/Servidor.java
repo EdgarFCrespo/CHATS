212cc8105b5462a179d678d869532af40425c73f
EdgarFCrespo <fuentescrespoe@gmail.com>
2017-11-20 19:51:27 -0600

    Se inician constructores y se crea un hilo

--- Chat/src/Servidor.java
+++ Chat/src/Servidor.java
@@ -1,3 +1,45 @@
+
+import java.awt.BorderLayout;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+
 public class Servidor {
-    
+    public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		MarcoServidor mimarco=new MarcoServidor();
+		
+		mimarco.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			
+		
+	}	
+
+    private static class MarcoServidor extends JFrame implements Runnable {
+
+        public MarcoServidor() 
+                setBounds(600,300,280,350);				
+			
+		JPanel milamina= new JPanel();
+		
+		milamina.setLayout(new BorderLayout());
+		
+		areatexto=new JTextArea();
+		
+		milamina.add(areatexto,BorderLayout.CENTER);
+		
+		add(milamina);
+		
+		setVisible(true);
+		
+		Thread mihilo=new Thread(this);
+		mihilo.start();
+
+        @Override
+        public void run() {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+		
+        }
+    }
 }
