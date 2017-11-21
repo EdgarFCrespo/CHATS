fa3df309986188721c13a5757459ef465bcab73f
EdgarFCrespo <fuentescrespoe@gmail.com>
2017-11-20 19:55:45 -0600

    Se crea un socket para entrada y otro para salida de datos

--- Chat/src/Servidor.java
+++ Chat/src/Servidor.java
@@ -1,5 +1,12 @@
 
 import java.awt.BorderLayout;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
@@ -37,7 +44,77 @@ public class Servidor {
 
         @Override
         public void run() {
-            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            // TODO Auto-generated method stub
+		//System.out.println("Ya escucha");
+		
+		try {
+			ServerSocket servidor=new ServerSocket(9999);
+			
+			ArrayList <String> listap=new ArrayList<String>();
+			String nick,ip,mensaje;
+			
+			Paquete_Envio paquete_recibido;
+		
+			
+			while(true) {
+			Socket misocket=servidor.accept();
+			
+			ObjectInputStream paquete_datos=new ObjectInputStream(misocket.getInputStream());
+			
+			paquete_recibido = (Paquete_Envio) paquete_datos.readObject();
+			
+			nick=paquete_recibido.getNick();
+			ip=paquete_recibido.getIp();
+			mensaje=paquete_recibido.getMensaje();
+			
+			//DataInputStream flujo_entrada=new DataInputStream(misocket.getInputStream());
+			//String mensaje_texto=flujo_entrada.readUTF();
+			//areatexto.append("\n" + mensaje_texto);
+			
+			
+			if(!mensaje.equals(" online")) {
+			areatexto.append("\n"+nick+":"+mensaje+" para: "+ip);
+			
+			Socket enviarDestino=new Socket(ip,9090);
+			ObjectOutputStream paqueteReenvio=new ObjectOutputStream(enviarDestino.getOutputStream());
+			
+			paqueteReenvio.writeObject(paquete_recibido);
+			paqueteReenvio.close();
+			enviarDestino.close();
+			
+			misocket.close();
+			}else {
+				
+				//=========================EN LINEA========================
+				
+				InetAddress localizacion=misocket.getInetAddress();
+				String ipremota=localizacion.getHostAddress();
+				System.out.println(ipremota);
+				
+				if(listap.contains(ipremota)){
+				}else {
+					listap.add(ipremota);
+				}
+				
+				paquete_recibido.setIps(listap);
+				
+				for(String z:listap) {
+					System.out.println("Array: " + z);
+					Socket enviarDestino=new Socket(z,9090);
+					ObjectOutputStream paqueteReenvio=new ObjectOutputStream(enviarDestino.getOutputStream());
+					paqueteReenvio.writeObject(paquete_recibido);
+					paqueteReenvio.close();
+					enviarDestino.close();
+					misocket.close();
+				}	
+							
+				//=========================================================
+			}
+			}
+		} catch (IOException | ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
         }
 		
         }
