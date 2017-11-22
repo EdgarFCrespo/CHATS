0fb819cd6cef2b551d4872d111a9fad6c5a27103
RociioH <32103075+RociioH@users.noreply.github.com>
2017-11-21 18:04:24 -0600

    importacion de librerias

--- Chat/src/Cliente.java
+++ Chat/src/Cliente.java
@@ -1,3 +1,19 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WinowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.net.InetAdrress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+import javax.swing.*;
+
 public class Cliente {
     public static void main(String[] args){
         
