39b751adc1ef0122005937e781e30b3ff35c9c7f
RociioH <32103075+RociioH@users.noreply.github.com>
2017-11-23 16:52:02 -0600

    clase Paquete_Envio con sets y gets

--- Chat/src/Cliente.java
+++ Chat/src/Cliente.java
@@ -155,3 +155,33 @@ class LaminaMarcoCliente extends JPanel implements Runnable{
 		}
 	}
 }
+
+class Paquete_Envio implements Serializable{
+	private String nick, ip, mensaje;
+	private ArrayList <String> ips;
+	
+	public ArrayList<String> getIps(){
+		return ips;
+	}
+	public void setIps(ArrayList<String> ips){
+		this.ips = ips;
+	}
+	public String getNick(){
+		return nick;
+	}
+	public void setNick(String nick){
+		this.nick = nick;
+	}
+	public String getIp(){
+		return ip;
+	}
+	public void setIp(String ip){
+		this.ip = ip;
+	}
+	public String getMensaje(){
+		return mensaje;
+	}
+	public void setMensaje(String mensaje){
+		this.mensaje = mensaje;
+	}
+}
